5cfb9653f128dbc350cb4e2e799ffcee148b2555
koslinj <koslinj@gmail.com>
2024-03-09 12:38:18 +0100

    half done About page

--- app/src/main/java/koslin/jan/calculator/About.kt
+++ app/src/main/java/koslin/jan/calculator/About.kt
@@ -3,7 +3,7 @@ package koslin.jan.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Author : AppCompatActivity() {
+class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_author)
